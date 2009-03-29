ce56d9f2241505b6d274f95948f0d8f87b892623
Uwe Finke <uwe@ufinke.de>
2009-03-29 19:26:12 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@4 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/config/DynamicElement.java
+++ src/de/ufinke/cubaja/config/DynamicElement.java
@@ -1,8 +1,28 @@
+// Copyright (c) 2009, Uwe Finke. All rights reserved.
+// Subject to BSD License. See "license.txt" distributed with this package.
+
 package de.ufinke.cubaja.config;
 
+/**
+ * Enables an element node for dynamic attributes and sub-elements.
+ * @author Uwe Finke
+ */
 public interface DynamicElement {
 
+  /**
+   * Returns an alternate name for a given XML element or attribute name.
+   * The result value may be <code>null</code>.
+   * @param originalName the original tag or attribute name
+   * @return a name this config node object could handle
+   * @throws ConfigException
+   */
   public String alternateName(String originalName) throws ConfigException;
   
+  /**
+   * Returns a <code>ParameterFactoryFinder</code> individual to an element node. 
+   * The result value may be <code>null</code>.
+   * @return a parameter factory finder
+   * @throws ConfigException
+   */
   public ParameterFactoryFinder parameterFactoryFinder() throws ConfigException;
 }
