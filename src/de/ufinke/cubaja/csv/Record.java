9a917b0def69099935235a0b21a5c2c7e06a9199
Uwe Finke <uwe@ufinke.de>
2009-05-01 11:05:53 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@36 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/csv/Record.java
+++ src/de/ufinke/cubaja/csv/Record.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2009, Uwe Finke. All rights reserved.
+// Subject to BSD License. See "license.txt" distributed with this package.
+
 package de.ufinke.cubaja.csv;
 
 public class Record {
