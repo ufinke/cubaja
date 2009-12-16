cd8747b25d05d2f120190631f23772ca4dc5ae92
Uwe Finke <uwe@ufinke.de>
2009-12-16 16:26:00 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@90 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/util/Stopwatch.java
+++ src/de/ufinke/cubaja/util/Stopwatch.java
@@ -3,6 +3,11 @@ package de.ufinke.cubaja.util;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * A stopwatch to measure elapsed time.
+ * <p>
+ * @author Uwe Finke
+ */
 public class Stopwatch {
 
   static private Log logger = LogFactory.getLog(Stopwatch.class);
@@ -10,11 +15,21 @@ public class Stopwatch {
   private long startMillis;
   private String message;
   
+  /**
+   * Takes the start time.
+   */
   public Stopwatch() {
   
     startMillis = System.currentTimeMillis();
   }
   
+  /**
+   * Takes the start time and logs a start message.
+   * <p>
+   * A subsequent call to <code>elapsedMillis</code>
+   * will also log an end message together with the elapsed time.
+   * @param message
+   */
   public Stopwatch(String message) {
     
     this();
@@ -27,6 +42,14 @@ public class Stopwatch {
     logger.debug(sb);
   }
   
+  /**
+   * Returns the elapsed time in milliseconds.
+   * This method may be called more than once.
+   * The start time is always the instantiation time of this object.
+   * If we have supplied a message in the constructor, 
+   * this method will log an end message together with the elapsed time.
+   * @return millis
+   */
   public long elapsedMillis() {
     
     long elapsed = System.currentTimeMillis() - startMillis;
@@ -43,6 +66,17 @@ public class Stopwatch {
     return elapsed;
   }
   
+  /**
+   * Formats a duration given in milliseconds.
+   * <p>
+   * The format is <code><i>N</i>d <i>N</i>h <i>N</i>m <i>N</i>s <i>N</i>ms</code>
+   * where <code>d</code> are days, <code>h</code> are hours, <code>m</code>
+   * are minutes, <code>s</code> are seconds and <code>ms</code> are milliseconds.
+   * Seconds and larger units are part of the result only when the value of the
+   * highest unit is not <code>0</code>. 
+   * @param millis
+   * @return formatted string
+   */
   public String format(long millis) {
     
     StringBuilder sb = new StringBuilder(32);
