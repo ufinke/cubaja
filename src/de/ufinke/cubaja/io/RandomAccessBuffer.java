fa28686e349edb23996b6c019ca795289c3235ab
Uwe Finke <uwe@ufinke.de>
2009-12-31 18:45:09 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@113 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/io/RandomAccessBuffer.java
+++ src/de/ufinke/cubaja/io/RandomAccessBuffer.java
@@ -3,12 +3,18 @@
 
 package de.ufinke.cubaja.io;
 
+import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutput;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
-public class RandomAccessBuffer implements DataOutput {
+public class RandomAccessBuffer implements DataInput, DataOutput {
 
   static private class Output extends OutputStream {
   
@@ -19,24 +25,47 @@ public class RandomAccessBuffer implements DataOutput {
       this.master = master;
     }
 
-    public void write(int b) {
+    public void write(int b) throws IOException {
 
       master.write(b);
     }
     
-    public void write(byte[] b, int off, int len) {
+    public void write(byte[] b, int off, int len) throws IOException {
       
       master.write(b, off, len);
     }
   }
   
+  static private class Input extends InputStream {
+    
+    private RandomAccessBuffer master;
+    
+    public Input(RandomAccessBuffer master) {
+      
+      this.master = master;
+    }
+
+    public int read() throws IOException {
+
+      return master.read();
+    }
+    
+    public int read(byte[] b, int off, int len) throws IOException {
+      
+      return master.read(b, off, len);
+    }
+  }
+  
   static private final int DEFAULT_CAPACITY = 4096;
   
-  private byte[] buffer;
+  protected byte[] buffer;
+  protected int position;
+  
   private int capacity;
   private int growthCapacity;
   private int size;
-  private int position;
+  
+  private Input inputStream;
   private Output outputStream;
   
   public RandomAccessBuffer() {
@@ -62,6 +91,12 @@ public class RandomAccessBuffer implements DataOutput {
   public void setPosition(int position) {
     
     this.position = position;
+    if (position > size) {
+      if (position > capacity) {
+        grow(position + 1);
+      }
+      size = position;
+    }
   }
   
   public int getPosition() {
@@ -74,7 +109,14 @@ public class RandomAccessBuffer implements DataOutput {
     return size;
   }
   
-  private int getNewPosition(int bytesToWrite) {
+  public byte[] toByteArray() {
+    
+    byte[] copy = new byte[size];
+    System.arraycopy(buffer, 0, copy, 0, size);
+    return copy;
+  }
+  
+  protected int newWritePosition(int bytesToWrite) throws IOException {
     
     int newPosition = position + bytesToWrite;
     if (newPosition > capacity) {
@@ -113,52 +155,52 @@ public class RandomAccessBuffer implements DataOutput {
     out.write(buffer, 0, size);
   }
 
-  public void write(int b) {
+  public void write(int b) throws IOException {
 
-    int pos = getNewPosition(1);
+    int pos = newWritePosition(1);
     buffer[position] = (byte) b;
     position = pos;
   }
 
-  public void write(byte[] b) {
+  public void write(byte[] b) throws IOException {
 
     write(b, 0, b.length);
   }
 
-  public void write(byte[] b, int off, int len) {
+  public void write(byte[] b, int off, int len) throws IOException {
 
-    int pos = getNewPosition(len);
+    int pos = newWritePosition(len);
     System.arraycopy(b, off, buffer, position, len);
     position = pos;
   }
 
-  public void writeBoolean(boolean v) {
+  public void writeBoolean(boolean v) throws IOException {
 
     write(v ? 1 : 0);
   }
 
-  public void writeByte(int v) {
+  public void writeByte(int v) throws IOException {
 
     write(v);
   }
 
-  public void writeShort(int v) {
+  public void writeShort(int v) throws IOException {
 
-    getNewPosition(2);
+    newWritePosition(2);
     int pos = position;
     buffer[pos++] = (byte) (v >> 8);
     buffer[pos++] = (byte) v;
     position = pos;
   }
 
-  public void writeChar(int v) {
+  public void writeChar(int v) throws IOException {
 
     writeShort(v);
   }
 
-  public void writeInt(int v) {
+  public void writeInt(int v) throws IOException {
 
-    getNewPosition(4);
+    newWritePosition(4);
     int pos = position;
     buffer[pos++] = (byte) (v >> 24);
     buffer[pos++] = (byte) (v >> 16);
@@ -167,9 +209,9 @@ public class RandomAccessBuffer implements DataOutput {
     position = pos;
   }
 
-  public void writeLong(long v) {
+  public void writeLong(long v) throws IOException {
 
-    getNewPosition(8);
+    newWritePosition(8);
     int pos = position;
     int n = (int) (v >> 32);
     buffer[pos++] = (byte) (n >> 24);
@@ -184,20 +226,20 @@ public class RandomAccessBuffer implements DataOutput {
     position = pos;
   }
 
-  public void writeFloat(float v) {
+  public void writeFloat(float v) throws IOException {
 
     writeInt(Float.floatToIntBits(v));
   }
 
-  public void writeDouble(double v) {
+  public void writeDouble(double v) throws IOException {
 
     writeLong(Double.doubleToLongBits(v));
   }
 
-  public void writeBytes(String s) {
+  public void writeBytes(String s) throws IOException {
 
     int len = s.length();
-    getNewPosition(len);
+    newWritePosition(len);
     int pos = position;
     for (int i = 0; i < len; i++) {
       buffer[pos++] = (byte) s.charAt(i);
@@ -208,7 +250,7 @@ public class RandomAccessBuffer implements DataOutput {
   public void writeChars(String s) throws IOException {
 
     int len = s.length();
-    getNewPosition(len);
+    newWritePosition(len);
     int pos = position;
     for (int i = 0; i < len; i++) {
       int c = s.charAt(i);
@@ -223,5 +265,159 @@ public class RandomAccessBuffer implements DataOutput {
     DataOutputStream dos = new DataOutputStream(getOutputStream());
     dos.writeUTF(s);
     dos.flush();
+  }
+  
+  protected int newReadPosition(int bytesToRead) throws IOException {
+    
+    int newPosition = position + bytesToRead;
+    if (newPosition > size) {
+      throw new EOFException();
+    }
+    return newPosition;
+  }
+  
+  public InputStream getInputStream() {
+    
+    if (inputStream == null) {
+      inputStream = new Input(this);
+    }
+    return inputStream;
+  }
+  
+  public int read() throws IOException {
+    
+    if (position == size) {
+      return -1;
+    }
+    return buffer[position++] & 0xFF;
+  }
+  
+  public int read(byte[] b, int off, int len) throws IOException {
+  
+    if (position == size) {
+      return -1;
+    }
+    
+    int copySize = Math.min(len, size - position);
+    System.arraycopy(buffer, position, b, off, copySize);
+    position = position + copySize;
+    return copySize;
+  }
+
+  public void readFully(byte[] b) throws IOException {
+
+    readFully(b, 0, b.length);
+  }
+
+  public void readFully(byte[] b, int off, int len) throws IOException {
+
+    int pos = newReadPosition(len);
+    System.arraycopy(buffer, position, b, off, len);
+    position = pos;
+  }
+
+  public int skipBytes(int n) throws IOException {
+
+    int skipped = Math.min(n, size - position);
+    position += skipped;
+    return skipped;
+  }
+
+  public boolean readBoolean() throws IOException {
+
+    return readByte() != 0;
+  }
+
+  public byte readByte() throws IOException {
+
+    int pos = newReadPosition(1);
+    byte result = buffer[position];
+    position = pos;
+    return result;
+  }
+
+  public int readUnsignedByte() throws IOException {
+
+    int pos = newReadPosition(1);
+    int result = buffer[position] & 0xFF;
+    position = pos;
+    return result;
+  }
+
+  public short readShort() throws IOException {
+
+    newReadPosition(2);
+    int pos = position;
+    int result = buffer[pos++] << 8;
+    result |= (buffer[pos++] & 0xFF);
+    position = pos;
+    return (short) result;
+  }
+
+  public int readUnsignedShort() throws IOException {
+
+    newReadPosition(2);
+    int pos = position;
+    int result = (buffer[pos++] & 0xFF) << 8;
+    result |= (buffer[pos++] & 0xFF);
+    position = pos;
+    return result;
+  }
+
+  public char readChar() throws IOException {
+
+    return (char) readUnsignedShort();
+  }
+
+  public int readInt() throws IOException {
+
+    newReadPosition(4);
+    int pos = position;
+    int result = buffer[pos++] << 24;
+    result |= (buffer[pos++] & 0xFF) << 16;
+    result |= (buffer[pos++] & 0xFF) << 8;
+    result |= (buffer[pos++] & 0xFF);
+    position = pos;
+    return result;
+  }
+
+  public long readLong() throws IOException {
+
+    newReadPosition(8);
+    int pos = position;
+    int high = buffer[pos++] << 24;
+    high |= (buffer[pos++] & 0xFF) << 16;
+    high |= (buffer[pos++] & 0xFF) << 8;
+    high |= (buffer[pos++] & 0xFF);
+    int low = buffer[pos++] << 24;
+    low |= (buffer[pos++] & 0xFF) << 16;
+    low |= (buffer[pos++] & 0xFF) << 8;
+    low |= (buffer[pos++] & 0xFF);
+    long result = low;
+    result |= (high << 32);
+    position = pos;
+    return result;
+  }
+
+  public float readFloat() throws IOException {
+
+    return Float.intBitsToFloat(readInt());
+  }
+
+  public double readDouble() throws IOException {
+
+    return Double.longBitsToDouble(readLong());
+  }
+
+  public String readLine() throws IOException {
+
+    BufferedReader d = new BufferedReader(new InputStreamReader(getInputStream()));
+    return d.readLine();
+  }
+
+  public String readUTF() throws IOException {
+
+    DataInputStream d = new DataInputStream(getInputStream());
+    return d.readUTF();
   }
 }
