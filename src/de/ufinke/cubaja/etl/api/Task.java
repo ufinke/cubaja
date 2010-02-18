87cdfa72fc88a02d36e343be7298a93a8d3b936d
Uwe Finke <uwe@ufinke.de>
2010-02-18 18:48:58 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@158 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/etl/api/Task.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2010, Uwe Finke. All rights reserved.
-// Subject to BSD License. See "license.txt" distributed with this package.
-
-package de.ufinke.cubaja.etl.api;
-
-public interface Task {
-
-  public TaskConfig getConfig();
-}
