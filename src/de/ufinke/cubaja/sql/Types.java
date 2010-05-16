b25422a8fd1c748878fe7d28b0c6c13ad92dfe44
Uwe Finke <uwe@ufinke.de>
2010-05-16 20:20:19 +0000

    git-svn-id: https://cubaja.googlecode.com/svn/trunk@249 dd2ac39a-1c43-11de-a027-6f5b8e30e268

--- src/de/ufinke/cubaja/sql/Types.java
+++ src/de/ufinke/cubaja/sql/Types.java
@@ -1,3 +1,6 @@
+// Copyright (c) 2010, Uwe Finke. All rights reserved.
+// Subject to BSD License. See "license.txt" distributed with this package.
+
 package de.ufinke.cubaja.sql;
 
 import java.io.InputStream;
